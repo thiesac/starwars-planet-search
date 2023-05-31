eb7fa2cca68eae8baeddcaec941e02250034cf49
Thiesa <thiesa.c@gmail.com>
2023-05-31 15:39:54 -0400

    useFetch retornando sem os residents

--- src/context/hooks/useFetch.js
+++ src/context/hooks/useFetch.js
@@ -3,10 +3,15 @@ import { useEffect, useState } from 'react';
 function useFetch() {
   const [data, setData] = useState([]);
 
-  const refresh = () => {
-    fetch('https://swapi.dev/api/planets')
-      .then((result) => result.json())
-      .then((receiveData) => console.log(receiveData.results));
+  const refresh = async () => {
+    const response = await fetch('https://swapi.dev/api/planets');
+    const receivedData = await response.json();
+    // console.log(receivedData.results)
+    const filteredResult = receivedData.results.map((planet) => {
+      delete planet.residents;
+      return planet;
+    });
+    setData(filteredResult);
   };
 
   useEffect(() => {
@@ -15,7 +20,6 @@ function useFetch() {
 
   return {
     data,
-    setData,
   };
 }
 
